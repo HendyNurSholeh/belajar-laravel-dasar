2039de7e0c23b348ba294e30bee4ac2d6cee197c
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2023-10-10 21:12:47 +0800

    learn testing (unit test and feature test

---
+++ tests/Unit/ProdukTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ProdukTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testContoh(){
+        $this->assertTrue(false);
+    }
+}
